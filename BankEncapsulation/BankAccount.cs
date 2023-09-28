79f81d9a1320f8247af5d1ea376651c31847329e
Will Royal <william.j.royal@gmail.com>
2023-09-28 15:43:21 -0700

    initial commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+      
+        }
+
+        private double balance = 0;
+
+        //Deposit method Encapsulation
+        public void Deposit(double amount)
+        {
+            balance = amount;
+        }
+
+        //Get Balance Method
+        public string GetBalance()
+        {
+            return $"${balance}";
+        }
+
+
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount willsAccount = new BankAccount();
+            Console.WriteLine("How much would you like to deposit?");
+            var amountToDep = double.Parse(Console.ReadLine());
+
+            willsAccount.Deposit(amountToDep);
+
+            Console.WriteLine($"Thank you, your account balance is now: {willsAccount.GetBalance()}");
         }
+        //In the main method of your application, create a new instance of the BankAccount class.
+        //Allow the user of the application to Deposit money and retrieve their balance through the console.
     }
 }
